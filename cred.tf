a3ca576fab57336c0d6bb1ad391821e7003ba0dc
map0450 <103323404+map0450@users.noreply.github.com>
2022-05-22 18:56:12 +0000

    Update cred.tf

--- cred.tf
+++ cred.tf
@@ -1,6 +1,12 @@
 provider "aws" {
-access_key = "AWS_LAB_ACCESS_KEY"
-secret_key = "AWS_LAB_SECRET_KEY"
-token = “AWS_LAB_SECURITY_TOKEN”
+  
+access_key = "ASIAYCDZFMTGL3ENJCJG"
+
+secret_key = "aA3UWOeWK8NeIcZWbghpgqdOyBIiYjyo9p23lzIK"
+
+token = "FwoGZXIvYXdzEHEaDPTPHOZEm5SEFyfW/iK4AVxhoIRINFyEdFeNCvG8jhrT3debl/S8ghxRQ208eQGKvkJo9wpf//x6F4jXNRRPFxXS6XzyOZrunHWfPA8sy0m0lLYXIrfDp3ue4m3cVw/CiDlliRD5g8/B9TDXce35L74v3ztWo5qTSLy/V31TgXL3vRRq/1ZfBIrxf1ipr7kfcH0pO+xFxNqDKFNcabqEXuwOTYXPHAu1AmfAkeMHktbPLnQ+LGPdKqzW0VDMWDiKX4+vpIPW5LIo27aplAYyLWEL9aXOGIXapeUgks4x4kSGMQvvBnZqdWuRGzTuu1qCAdPBgjZNxnKhPK0WAA=="
+
 region = "us-east-1"
- }  
+
+ }
+
