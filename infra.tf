219860a62313f34b1786b64d566a365721f39e52
map0450 <103323404+map0450@users.noreply.github.com>
2022-05-22 18:57:18 +0000

    Create infra.tf

---
+++ infra.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "firstproject" {
+  
+  ami           = "ami-2757f631"
+
+  instance_type = "t2.micro"
+
+}
+                                                                                                                                      
